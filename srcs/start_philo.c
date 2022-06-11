a514862ee3c516e7f36a78484847992e169f7769
pareidolies <sacha.mkara@gmail.com>
2022-06-11 13:40:24 +0200

    starting thread

--- srcs/start_philo.c
+++ srcs/start_philo.c
@@ -1,3 +1,24 @@
 # include "../includes/philosophers.h"
 
+void	thanatos_thread(t_data *data)
+{
 
+
+}
+
+void	philo_thread(t_data *data)
+{
+
+
+}
+
+int	end_philo(t_data *data)
+{
+
+
+}
+
+int	start_philo(t_data *data)
+{
+		
+}
